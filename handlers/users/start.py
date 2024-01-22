1d5ec1e197dd72862550991409e72d35677fd590
Jasurbek <jasurbekodilovn@gmail.com>
2024-01-22 10:50:56 +0500

    sql baza qoshildi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,10 +1,32 @@
+import sqlite3
 from aiogram import types
 from keyboards.default.menu import menu
-from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher.filters.builtin import CommandStart, Command
 
-from loader import dp
+from data.config import ADMINS
+from loader import dp, db, bot
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!", reply_markup=menu)
+    name = message.from_user.full_name
+    # Foydalanuvchini bazaga qo'shamiz
+    try:
+        db.add_user(id=message.from_user.id,
+                    name=name)
+    except sqlite3.IntegrityError as err:
+        await bot.send_message(chat_id=ADMINS[0], text=err)
+
+    await message.answer("Xush kelibsiz!", reply_markup=menu)
+    # Adminga xabar beramiz
+    count = db.count_users()[0]
+    msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+    await bot.send_message(chat_id=ADMINS[0], text=msg)
+
+@dp.message_handler(Command('Users'))
+async def user_soni(msg: types.Message):
+    count = db.count_users()[0]
+    message = f"{msg.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+    await bot.send_message(chat_id=ADMINS[0], text=message)
+
+
